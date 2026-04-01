fa4bffaf0f9835800087f30c7acfa39a2199509f
hPedro11 <lima13774@gmail.com>
2026-04-01 16:42:59 -0300

    Projeto 1 + Comandos Git

--- Projetos/Projeto 1/main.py
+++ Projetos/Projeto 1/main.py
@@ -5,3 +5,11 @@
 # Para agilizar o processo, João quer um programa que receba o valor total da conta e a porcentagem de gorjeta desejada e exiba o valor final que o cliente deverá pagar.
 # Crie um programa que peça ao usuário o valor da conta e a porcentagem de gorjeta. O programa deve calcular e exibir o valor da gorjeta e o total a ser pago.
 
+def cal_conta():
+    vl_conta = float(input("Digite o valor da conta: "))
+    gorjeta = float(input("Digite a porcentagem da gorjeta: "))
+    vl_gorjeta= vl_conta * (gorjeta / 100)
+    vl_total = vl_conta + vl_gorjeta
+    print(f"O valor da gorjeta foi: {vl_gorjeta}\nTotal à ser pago é: {vl_total}")
+
+cal_conta()
